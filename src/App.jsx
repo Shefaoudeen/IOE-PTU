f0b0e0a88e6a92faa91ac61cb8e65880bcec515c
Shefa <shefaoudeen123@gmail.com>
2024-01-01 13:22:30 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Bearers from "./pages/Bearers";
 import Contact from "./pages/Contact";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import LogoMobile from "./pages/Logo";
 
 export default function App() {
   Aos.init({
@@ -16,6 +17,7 @@ export default function App() {
 
   return (
     <div className="relative">
+      <LogoMobile />
       <Navbar />
       <Home />
       <Past />
---
+++ src/pages/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Logo } from "../assets";
+
+const LogoMobile = () => {
+  return (
+    <div className="fixed flex justify-center items-center w-full h-screen -z-10 sm:hidden">
+      <img src={Logo} className="opacity-10" />
+    </div>
+  );
+};
+
+export default LogoMobile;
