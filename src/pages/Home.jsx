1f8878218088c9ade642c492cc42369b2db70d81
Shefa <shefaoudeen123@gmail.com>
2024-02-10 22:06:52 +0530

    added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css";
 
 import { Image1, Wallpaper2, IOT1, IOT2, Photo1, IOT3 } from "../assets";
 
@@ -10,8 +11,8 @@ const Home = () => {
     >
       <div className="w-max-[100vw] text-center relative w-full h-screen flex flex-col  text-black max-sm:h-[150dvh] max-sm:-mb-[200px]">
         <div className=" top-0 left-0 right-0 w-max-[100vw] h-[50dvh] max-sm:h-[25%] z-10 ">
-          <div className="relative w-full h-full">
-            <div className="absolute w-full h-full top-0 bottom-0 bg-gradient-to-tr max-sm:bg-white from-black via-gray-900 to-gray-300"></div>
+          <div className="relative w-full h-full background-image">
+            <div className="absolute w-full h-full top-0 bottom-0  max-sm:bg-white "></div>
             <img
               src={Wallpaper2}
               className="w-full h-[50dvh] object-cover  shadow-inner-[100px] absolute opacity-70 max-sm:h-[100%]"
